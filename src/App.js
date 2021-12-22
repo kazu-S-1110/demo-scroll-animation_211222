9b2a4d0e875356e926c135345f7f0b5685962f19
kazu-S-1110 <67362767+kazu-S-1110@users.noreply.github.com>
2021-12-22 12:53:59 +0900

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import ScrollReveal from 'scrollreveal';
+import ScrollRevealContainer from './ScrollRevealContainer';
 
 function App() {
+  ScrollReveal().reveal('.App');
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div style={{ marginBottom: '400px' }}></div>
+      <ScrollRevealContainer move="right">
+        <h1>右から登場</h1>
+        <h2>Edit to see some magic happen!</h2>
+      </ScrollRevealContainer>
+      <ScrollRevealContainer move="left">
+        <h1>左から登場</h1>
+        <h2>Edit to see some magic happen!</h2>
+      </ScrollRevealContainer>
+      <ScrollRevealContainer move="top">
+        <h1>上から登場</h1>
+        <h2>Edit to see some magic happen!</h2>
+      </ScrollRevealContainer>
+      {/* ↓props「move」に何も指定しなければ「origin」は「bottom」になる */}
+      <ScrollRevealContainer>
+        <h1>下から登場</h1>
+        <h2>Edit to see some magic happen!</h2>
+      </ScrollRevealContainer>
     </div>
   );
 }
---
+++ src/ScrollRevealContainer.jsx
@@ -0,0 +1,27 @@
+import { useRef, useEffect } from 'react';
+import scrollReveal from 'scrollreveal';
+
+const ScrollRevealContainer = ({ children, move }) => {
+  const sectionRef = useRef(null);
+
+  useEffect(() => {
+    if (sectionRef.current)
+      scrollReveal().reveal(sectionRef.current, {
+        reset: true,
+        delay: 400,
+        opacity: 0,
+        origin:
+          move === 'left'
+            ? 'left'
+            : move === 'right'
+            ? 'right'
+            : move === 'top'
+            ? 'top'
+            : 'bottom',
+        distance: '40px',
+      });
+  }, [sectionRef]);
+
+  return <section ref={sectionRef}>{children}</section>;
+};
+export default ScrollRevealContainer;
